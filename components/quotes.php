16f1cb710ccaa2ceb3839c1d1f1c3387716694ea
Ivan Erofeev <ierofeev@syntellect.ru>
2022-06-28 18:50:43 +0300

    init

---
+++ components/quotes.php
@@ -0,0 +1,16 @@
+<section class="quotes container">
+  <h2 class="section-title">Любимые цитаты</h2>
+  <blockquote>
+    <p>Сказал как зарезал.</p>
+    <cite>Стах Рубин</cite>
+  </blockquote>
+  <blockquote>
+    <p>Ты врач, а я знахарь. Ты врёшь, а я знаю.</p>
+    <cite>Артемий Бурах</cite>
+  </blockquote>
+  <blockquote>
+    <p>Не повторите ошибок Сонной Группы, мой друг. Не просыпайтесь.</p>
+    <cite>Юлия Люричева</cite>
+  </blockquote>
+  
+</section>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Мое жабохранилище';
+$name = 'Опознанный жабич';
+$image = 'img/user2.webp';
+$email = 'i.eroffeev@yandex.ru';
+$phone = '+79995552525';
+
+require('components/header-mini.php');
+require('components/about.php');
+//require('components/status.php');
+//require('components/photos.php');
+//require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer-logo.php');
